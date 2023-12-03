f9f8c23031f808ef3322058144278897ad3d2167
Ander Juaristi <a@juaristi.eus>
2023-12-03 21:17:39 +0100

    Fix coding style & examples

--- examples/http3_send_request.c
+++ examples/http3_send_request.c
@@ -6,49 +6,43 @@
 #include <wget.h>
 #include <string.h>
 
-int main(void){
-    const char *hostname = "quic.nginx.org";
-    char *data = NULL;
-    wget_iri *uri;
-    wget_http_request *req;
-    wget_http_connection *http3 = NULL;
+int main(void) {
+	const char *hostname = "quic.nginx.org";
+	char *data = NULL;
+	wget_iri *uri;
+	wget_http_request *req;
+	wget_http_connection *http3 = NULL;
 
-<<<<<<< HEAD
 	wget_logger_set_stream(wget_get_logger(WGET_LOGGER_DEBUG), stderr);
 	wget_logger_set_stream(wget_get_logger(WGET_LOGGER_ERROR), stderr);
 	wget_logger_set_stream(wget_get_logger(WGET_LOGGER_INFO), stdout);
-=======
-wget_logger_set_stream(wget_get_logger(WGET_LOGGER_DEBUG), stderr);
-wget_logger_set_stream(wget_get_logger(WGET_LOGGER_ERROR), stderr);
-wget_logger_set_stream(wget_get_logger(WGET_LOGGER_INFO), stdout);
->>>>>>> 831ec624 (Remove unused wget_ssl_init_quic)
 
-    uri = wget_iri_parse(hostname, NULL);
+	uri = wget_iri_parse(hostname, NULL);
 
-    req = wget_http_create_request(uri, "GET");
-    uri->port = 443;
-    wget_http_add_header(req, "user-agent", "hello-client");
+	req = wget_http_create_request(uri, "GET");
+	uri->port = 443;
+	wget_http_add_header(req, "user-agent", "hello-client");
 
-    int ret = wget_http3_open(&http3, uri);
-    if (ret >= 0){
-        if (wget_http3_send_request(http3, req) == 0){
-            wget_http_response *resp = wget_http3_get_response(http3);
-            if (!resp){
-                wget_http_free_request(&req);
-                wget_http3_close(&http3);
-                return -1;
-            }
-            fprintf(stdout, "%s", resp->body->data);
-            wget_http_free_response(&resp);
-            wget_http_free_request(&req);
-        }else{
-            wget_http_free_request(&req);
-            wget_http3_close(&http3);
-            return -1;
-        }
-    } else{
-        return -1;
-    }
-    wget_http3_close(&http3);
-    return 0;
+	int ret = wget_http3_open(&http3, uri);
+	if (ret >= 0) {
+		if (wget_http3_send_request(http3, req) == 0) {
+			wget_http_response *resp = wget_http3_get_response(http3);
+			if (!resp) {
+				wget_http_free_request(&req);
+				wget_http3_close(&http3);
+				return -1;
+			}
+			fprintf(stdout, "%s", resp->body->data);
+			wget_http_free_response(&resp);
+			wget_http_free_request(&req);
+		} else {
+			wget_http_free_request(&req);
+			wget_http3_close(&http3);
+			return -1;
+		}
+	} else {
+		return -1;
+	}
+	wget_http3_close(&http3);
+	return 0;
 }
--- examples/quic_write.c
+++ examples/quic_write.c
@@ -6,36 +6,34 @@
 #include <wget.h>
 #include <string.h>
 
-int main(void){
-    int ret;
-    const uint16_t port = 5556;
-    const char *hostname = "localhost";
+int main(void) {
+	int ret;
+	const uint16_t port = 5556;
+	const char *hostname = "localhost";
 
 	wget_quic *quic = wget_quic_init();
-	if (!quic){
+	if (!quic) {
 		fprintf(stderr, "Error in wget_quic_init()\n");
+		return 1;
 	}
 
 	const char *key_path = "/home/hmk/wget2/examples/credentials/ca.pem";
-    wget_ssl_quic_set_config_string(WGET_SSL_CA_FILE, key_path);
-    wget_quic_set_ssl_hostname(quic, hostname);
+	wget_ssl_set_config_string(WGET_SSL_CA_FILE, key_path);
+	wget_quic_set_ssl_hostname(quic, hostname);
 
-
-    ret = wget_quic_connect(quic, hostname, port);
-	if (ret < 0){
+	ret = wget_quic_connect(quic, hostname, port);
+	if (ret < 0) {
 		fprintf(stderr, "Error in wget_quic_connect()\n");
 		wget_quic_deinit(&quic);
 	}
 
 	wget_quic_stream *stream = wget_quic_stream_init_bidirectional(quic);
-	if (!stream){
-		return -1;
-	}
+	if (!stream)
+		return 1;
 	const char *data = "Hello World!";
 	ret = wget_quic_stream_push(stream, data, strlen(data), REQUEST_BYTE);
-	if (ret < 0){
+	if (ret < 0)
 		return ret;
-	}
 	ret = wget_quic_write(quic, stream);
-    return ret;
+	return ret;
 }
