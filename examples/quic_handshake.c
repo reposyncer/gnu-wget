63a4c0751106e526e8473550caf20bff0e7bec44
Ander Juaristi <a@juaristi.eus>
2023-12-03 21:17:39 +0100

    Fix coding style & examples

--- examples/quic_handshake.c
+++ examples/quic_handshake.c
@@ -4,26 +4,26 @@
 
 #include <wget.h>
 
-int main(void){
-    int ret;
-    const uint16_t port = 5556;
-    const char *hostname = "localhost";
+int main(void) {
+	int ret;
+	const uint16_t port = 5556;
+	const char *hostname = "localhost";
 
 	wget_quic *quic = wget_quic_init();
-	if (!quic){
+	if (!quic) {
 		fprintf(stderr, "Error in wget_quic_init()\n");
+		return 1;
 	}
 
 	const char *key_path = "/home/hmk/wget2/examples/credentials/ca.pem";
-    wget_ssl_quic_set_config_string(WGET_SSL_CA_FILE, key_path);
-    wget_quic_set_ssl_hostname(quic, hostname);
+	wget_ssl_set_config_string(WGET_SSL_CA_FILE, key_path);
+	wget_quic_set_ssl_hostname(quic, hostname);
 
-
-    ret = wget_quic_connect(quic, hostname, port);
-	if (ret < 0){
+	ret = wget_quic_connect(quic, hostname, port);
+	if (ret < 0) {
 		fprintf(stderr, "Error in wget_quic_connect()\n");
 		wget_quic_deinit(&quic);
 	}
 
-    return 0;
+	return 0;
 }
