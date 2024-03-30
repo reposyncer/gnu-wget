b3e5938f646240dbd8fb3164c20281c27ad08edc
harshmohan07 <code.harshmohan@gmail.com>
2024-03-30 17:46:20 +0530

    Issue related to wget_http3_close resolved [skip ci]
    Multiple calls to wget_http3_close happen which resulted in Segmentation Faults
    Resolved it by setting downloader->conn to NULL

--- libwget/http3.c
+++ libwget/http3.c
@@ -479,11 +479,12 @@ void wget_http3_close(wget_http_connection **h3)
 {
 	wget_http_connection *http3 = *h3;
 	if (http3) {
-		/* nghttp3_conn_del(http3->conn); */
+		nghttp3_conn_del(http3->conn);
 		wget_quic_close(http3->quic);
-		/* wget_quic_deinit(&http3->quic); */
+		wget_quic_deinit(&http3->quic);
 		xfree(http3->http3_ctx);
-		/* xfree(http3); */
+		xfree(http3);
+		*h3 = NULL;
 	}
 }
 #else
